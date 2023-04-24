e322e98582c60615e29e02f12d8747f09d3de03e
Brigant <sergeyjuly@gmail.com>
2023-04-24 06:33:17 +0300

    Fix: migrations

---
+++ backend/migrations/5_create_list_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "list" (
+    "id" uuid DEFAULT gen_random_uuid() NOT NULL,
+    "movie_id" uuid NOT NULL,
+    "account_id" uuid NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "created" Timestamp With Time Zone NOT NULL DEFAULT NOW(),
+    "modified" Timestamp With Time Zone NOT NULL DEFAULT NOW(),
+    PRIMARY KEY ("id"),
+    CONSTRAINT "unique_list_id" UNIQUE("id"),
+    CONSTRAINT "unique_account_id_type" UNIQUE("account_id", "type"),
+    CONSTRAINT "movie_fk" FOREIGN KEY (movie_id) REFERENCES public."movie"(id),
+    CONSTRAINT "account_fk" FOREIGN KEY (account_id) REFERENCES public."account"(id)
+);
+
+CREATE TRIGGER update_list_modtime 
+BEFORE UPDATE ON "list" 
+FOR EACH ROW EXECUTE PROCEDURE  update_modified_column();
